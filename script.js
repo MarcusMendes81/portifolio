4f86bed86b8b65d67733a4e329f1fd4bc2744365
MarcusMendes81 <80123680+MarcusMendes81@users.noreply.github.com>
2023-05-19 00:27:28 -0300

    AA

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ text = "Olá mundo! Seja bem vindo(a) ao meu portifólio simplório! ";
 
 var botao = document.getElementById("botao");
 var lorem = document.getElementById("lorem");
-var botao_home = document.getElementById("bt_home");
 
 
+//função de escrita
 typing = () => {
     if(i < text.length){
       
@@ -16,21 +16,45 @@ typing = () => {
         setTimeout(typing,100);
     }   
 }  
-
-
 clearingUp = () =>{
     botao.addEventListener( "click", () =>{
         lorem.innerHTML = "";
     })
 }
-
-
-
-
 typing();
 //clearingUp();
 
 
+// carousel
+var carousel = document.querySelector('.projetos_academicos');
+var carouselItems = document.querySelectorAll('.conteudos');
+var prevButton = document.querySelector('.voltar');
+var nextButton = document.querySelector('.proximo');
+var atual = 0;
+var itemWidth = carouselItems[0].offsetWidth;
 
+function showItem(index) {
+  var offset = -index * itemWidth;
+  carousel.style.transform = 'translateX(' + offset + 'px)';
+}
+
+function nextItem() {
+  atual++;
+  if (atual >= carouselItems.length) {
+    atual = 0;
+  }
+  showItem(atual);
+}
+
+function prevItem() {
+  atual--;
+  if (atual < 0) {
+    atual = carouselItems.length - 1;
+  }
+  showItem(atual);
+}
+
+nextButton.addEventListener('click', nextItem);
+prevButton.addEventListener('click', prevItem);
 
 
